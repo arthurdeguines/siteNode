39842d8ba4505c42f32fa67f82efc00f2596d6db
Arthur <deguinesarthur@gmail.com>
2018-01-10 14:25:03 +0100

    undefined

--- ExpressApp1/routes/postregister.js
+++ ExpressApp1/routes/postregister.js
@@ -4,10 +4,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function (req, res) {
+    var erreur = 0;
     const db = require('./dataBase');
-    console.log(req.body.register_name);
-    db.connection("INSERT INTO utilisateur (login, mdp, email,nom,prenom) VALUES ('" + req.body.register_name + "', '" + req.body.register_mdp + "', '" + req.body.register_email + "', 'test', 'test')");
-    res.render('post.ejs');
+    console.log(req.body.register_name.length);
+    if (req.body.register_name.length < 5) {
+        erreur = 1;
+    }
+    if (erreur == 0) {
+        db.connection("INSERT INTO utilisateur (login, mdp, email,nom,prenom) VALUES ('" + req.body.register_name + "', '" + req.body.register_mdp + "', '" + req.body.register_email + "', 'test', 'test')");
+    }
+        res.render('post.ejs');
 });
 
 module.exports = router;
